9ce6b28b5dc0dd5e4db583d6600cc43a4353f137
constanzagra <riverosaconstanza@gmail.com>
2025-03-18 20:14:43 -0300

    fixing client error

--- client.js
+++ client.js
@@ -12,25 +12,26 @@ const rl = readline.createInterface({
 
 const client = net.createConnection({ host: HOST, port: PORT }, () => {
     console.log('Connecting to server');
-   promptUser();
+    promptUser();
 });
 
 client.on('data', (data) => {
-    console.log('Servers Answer: ', data.toString().trim());
-   // yesNoPromt();
+    console.log('\nServer Answer: ', data.toString().trim());
+    yesNoPromt();
 });
 
 client.on('error', (err) => {
-    console.error(`⚠️ Connection error: ${err.message}`)
+    console.error(`\n❌  Connection error, couldn't connect to server: ${err.message}`)
 });
 
 client.on('end', () => {
-    console.log('Disconnected from server');
+    console.log('\n⚠️  Disconnected from server');
     process.exit();
 });
 
 function addBookPrompt(){
     rl.question("Please insert the book title: ", (bookTitle) => {
+        
         rl.question("Please insert the author: ", (bookAuthor) => {
             const addBookInput = `ADD BOOK + ${bookTitle} + ${bookAuthor}` 
             client.write(addBookInput)
@@ -40,17 +41,18 @@ function addBookPrompt(){
 
 function promptUser() {
     console.log("\n****************************");
-    console.log("  📚 AVAILABLE COMMANDS:");
+    console.log("  ✅ AVAILABLE COMMANDS:");
     console.log("******************************");
-    console.log("  👥 GET AUTHORS     → Get authors' list");
-    console.log("  ✍️ ADD AUTHOR      → Add an author (name, nationality)");
-    console.log("  🏛️ GET PUBLISHERS  → Get publishers' list");
-    console.log("  🏢 ADD PUBLISHER   → Add publisher (name, location)");
-    //Falta console.log("SEARCH PUBLISHER" -> Search publisher (name, location)); 
-    console.log("  📚 GET BOOKS       → Get books' list");
-    console.log("  ➕ ADD BOOK        → Add a book (title, author)");
-    console.log("  🔍 SEARCH BOOK BY TITLE → Search a book by title");
-    console.log("  👋 EXIT to finish");
+    console.log("  ✍️  GET AUTHORS          → Get authors list");
+    console.log("  ➕ ADD AUTHOR           → Add an author (name, nationality)");
+    console.log("  ✔️SEARCH AUTHOR        → Search an author by (name author o nationality)");
+    console.log("  ✍️  GET PUBLISHERS       → Get publishers list");
+    console.log("  ➕ ADD PUBLISHER        → Add publisher (name)");
+    console.log("  ✔️  SEARCH PUBLISHER     → Search a publisher by (name or location)");
+    console.log("  ✍️  GET BOOKS            → Get books list");
+    console.log("  ➕ ADD BOOK             → Add a book (title, author)");
+    console.log("  ✔️  SEARCH BOOK BY TITLE → Search a book by title");
+    console.log("  ❌ EXIT to finish");
     console.log("*******************************");
 
     rl.question('Please insert a command: ', (input) => {
@@ -68,9 +70,11 @@ function promptUser() {
 };
 
 function yesNoPromt() {
-    if(keyInYN('Would you like to continue? (Y/N)')){
-        promptUser()        
-    }else{
-        client.end();
-    }
-};
+    rl.question('Would you like to continue? (Y/N) ', (answer) => {
+        if (answer.toUpperCase() === 'Y') {
+            promptUser();
+        } else {
+            client.end();
+        }
+    });
+}
